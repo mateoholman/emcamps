ed151e308c2fdb4c01be549fa207169da7079060
Matthew Holman <mateoholman@gmail.com>
2017-08-22 14:29:28 -0500

    Added passport and strategy to server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 app.listen(PORT);
