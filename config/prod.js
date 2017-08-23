9e3b28ef0a3e0e2c30e30ddd25083e6901bb8adb
Matthew Holman <mateoholman@gmail.com>
2017-08-23 16:54:04 -0500

    Setup logic to differentiate between dev and prod environments

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//Production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  MONGODB_URI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
