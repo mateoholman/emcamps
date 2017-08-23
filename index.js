ced424502ab0381d6606ef05974143fd352eda75
Matthew Holman <mateoholman@gmail.com>
2017-08-22 20:38:20 -0500

    Finished oauth portion of authentication

--- index.js
+++ index.js
@@ -3,26 +3,34 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const keys = require('./config/keys');
+
+//Step 0: The authorization Google needs when making an auth request
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callBackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('Access token: ', accessToken);
+      console.log('Refresh token: ', refreshToken);
+      console.log('Profile: ', profile);
     }
   )
 );
 
+//Step 1: Forward the User's request to Google
 app.get(
   '/auth/google',
   passport.authenticate('google', {
     scope: ['profile', 'email']
   })
 );
+
+//Step 2: Take Google's access token and re-route the user
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
