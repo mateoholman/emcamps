61102ad3b6696c6dbc630f7a41959002b1f13931
Matthew Holman <mateoholman@gmail.com>
2017-08-28 14:05:00 -0500

    Setup route handling for production

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Serve production assets when in production!
+  app.use(express.static('client/build'));
+  //Serve the index.html file if Express doesn't recognize the route.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
