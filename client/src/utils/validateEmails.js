1119a300b3ebd87800e76b7311e70f943eb2fc76
Matthew Holman <mateoholman@gmail.com>
2017-08-30 13:50:57 -0500

    Created util to help validate emails

---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,13 @@
+const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim().filter(email => emailRe.test(email) === false));
+
+  if (invalidEmails.length) {
+    return `These e-mails are invalid: ${invalidEmails}`;
+  } else {
+    return null;
+  }
+};
