a840f69e24da0881c2482d0db0c7eace2b1d8a35
Matthew Holman <mateoholman@gmail.com>
2017-08-30 14:03:56 -0500

    Fixed bugs & warnings with the regex

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line
 const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default emails => {
+  emails = emails.replace(/,\s*$/, '');
   const invalidEmails = emails
     .split(',')
-    .map(email => email.trim().filter(email => emailRe.test(email) === false));
+    .map(email => email.trim())
+    .filter(email => emailRe.test(email) === false);
 
   if (invalidEmails.length) {
     return `These e-mails are invalid: ${invalidEmails}`;
