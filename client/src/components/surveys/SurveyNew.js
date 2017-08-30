1c0dd5b430d5e6233065ef9401258c9b657e8f33
Matthew Holman <mateoholman@gmail.com>
2017-08-30 14:30:43 -0500

    Setup the back button for form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,18 @@
 //SurveyNew holds the SurveyForm and SurveyReview
-
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.state.showFormReview
+          ? <SurveyFormReview />
+          : <SurveyForm
+              onSurveySubmit={() => this.setState({ showFormReview: true })}
+            />}
       </div>
     );
   }
