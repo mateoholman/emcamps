1df78113d62a4528c86998ab60ff1c12b125baf5
Matthew Holman <mateoholman@gmail.com>
2017-09-01 13:27:06 -0500

    Refactored and added lastResponded field

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your feedback!');
   });
 
@@ -28,15 +28,19 @@ module.exports = app => {
       .uniqBy('email', 'surveyId')
       //Execute a MongoDB query for each event we want to update
       .each(({ surveyId, email, choice }) => {
-        Survey.updateOne({
-          _id: surveyId,
-          recipients: {
-            $elemMatch: { email: email, responded: false }
-          }, {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }
-        }).exec();
+        ).exec();
       })
       .value();
 
