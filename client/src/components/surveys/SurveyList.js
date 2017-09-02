36b5160de5d9411b3b6eff31cf8068ed50efedc5
Matthew Holman <mateoholman@gmail.com>
2017-09-02 11:06:32 -0500

    Updated styling

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,10 +8,10 @@ class SurveyList extends Component {
   }
 
   renderSurveys() {
-    return this.props.surveys.map(survey => {
+    return this.props.surveys.reverse().map(survey => {
       return (
-        <div className="card darken-1" key={survey._id}>
-          <div className="card-content">
+        <div className="card blue-grey darken-2" key={survey._id}>
+          <div className="card-content white-text">
             <span className="card-title">
               {survey.title}
             </span>
